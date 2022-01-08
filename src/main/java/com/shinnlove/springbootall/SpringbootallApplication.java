1ed9dfcdf6c92299311159e6116e8b6d152e123d
zhaochensheng <306086640@qq.com>
2022-01-08 19:19:18 +0800

    initialize springboot project

---
+++ src/main/java/com/shinnlove/springbootall/SpringbootallApplication.java
@@ -0,0 +1,13 @@
+package com.shinnlove.springbootall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootallApplication.class, args);
+    }
+
+}
