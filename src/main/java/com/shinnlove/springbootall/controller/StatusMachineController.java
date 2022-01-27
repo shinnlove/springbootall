4b9bda0a7dd8393311a6919e21ddf3d493225d02
zhaochensheng <306086640@qq.com>
2022-01-27 17:11:23 +0800

    [feature/status_machine_upgrade] 1/27
    features:
    
    1. store latest status machine.

--- src/main/java/com/shinnlove/springbootall/controller/StatusMachineController.java
+++ src/main/java/com/shinnlove/springbootall/controller/StatusMachineController.java
@@ -4,8 +4,14 @@
  */
 package com.shinnlove.springbootall.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.shinnlove.springbootall.process.enums.ActionType;
+import com.shinnlove.springbootall.process.model.context.DataContext;
+import com.shinnlove.springbootall.process.no.SnowflakeIdWorker;
+import com.shinnlove.springbootall.process.service.StatusMachineService;
 
 /**
  * Test status machine controller.
@@ -17,8 +23,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/status")
 public class StatusMachineController {
 
+    @Autowired
+    private StatusMachineService statusMachineService;
+
+    @Autowired
+    private SnowflakeIdWorker    snowflakeIdWorker;
+
     @RequestMapping("/proceed")
     public String proceedProcess(String name) {
+
+        int actionId = ActionType.AUTO_ONLINE.getActionId();
+        long refUniqueNo = snowflakeIdWorker.nextId();
+        String info = "this is remark";
+        DataContext<String> dataContext = new DataContext<>(info);
+
+        statusMachineService.proceedProcess(actionId, refUniqueNo, dataContext);
+
         return "hello " + name;
     }
 
