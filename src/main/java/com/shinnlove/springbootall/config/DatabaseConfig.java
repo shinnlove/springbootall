6f9782fef5005c277cb160a0ab19f51b383f78a0
p_cshzhao <p_cshzhao@tencent.com>
2024-04-15 19:26:37 +0800

    add transaction template.

--- src/main/java/com/shinnlove/springbootall/config/DatabaseConfig.java
+++ src/main/java/com/shinnlove/springbootall/config/DatabaseConfig.java
@@ -14,6 +14,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.sql.DataSource;
 
@@ -48,8 +50,13 @@ public class DatabaseConfig {
     }
 
     @Bean(name = "transactionManager")
-    public DataSourceTransactionManager dataSourceTransactionManager() {
-        return new DataSourceTransactionManager(dataSource());
+    public DataSourceTransactionManager dataSourceTransactionManager(@Qualifier("dataSource") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean(name = "transactionTemplate")
+    public TransactionTemplate transactionTemplate(@Qualifier("transactionManager") DataSourceTransactionManager dataSourceTransactionManager) {
+        return new TransactionTemplate(dataSourceTransactionManager);
     }
 
     @Bean(name = "sqlSessionFactory")
